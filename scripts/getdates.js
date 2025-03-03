30bfe11e3af26f7f9cac77bfcd9f4136e2fd764e
Abinadi2007 <josephabinadi@gmail.com>
2025-03-03 17:30:53 -0600

    modified:   scripts/getdates.js

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,5 +1,5 @@
 const currentYear = new Date().getFullYear();
-document.getElementById("copyright-year").textContent = currentYear;
+document.getElementById("currentyear").textContent = currentYear;
 
 const lastModified = document.lastModified;
 document.getElementById("last-modified").textContent = lastModified;
